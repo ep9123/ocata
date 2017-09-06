86b0b0ee735f7adf17c7a6ce72d279fc66f32b8e
ep9123 <peach@att.com>
2017-09-06 20:29:19 +0100

    [DBaaS-2017-09-06] update environment.sh and sql.sh

--- environment.sh
+++ environment.sh
@@ -1,4 +1,5 @@
 #
+set -x
 # setup environment
 # As root
 
@@ -7,14 +8,14 @@
 #echo 'Acquire::https::proxy "http://one.proxy.att.com:8080/";' >> /etc/apt/apt.conf
 
 # proxy.sh
-echo "export http_proxy=http://one.proxy.att.com:8080"          >> /etc/profile.d/proxy.sh
-echo "export https_proxy=http://one.proxy.att.com:8080"         >> /etc/profile.d/proxy.sh
-echo "export no_proxy=127.0.0.1,10.0.2.15,localhost"            >> /etc/profile.d/proxy.sh
+#echo "export http_proxy=http://one.proxy.att.com:8080"          >> /etc/profile.d/proxy.sh
+#echo "export https_proxy=http://one.proxy.att.com:8080"         >> /etc/profile.d/proxy.sh
+#echo "export no_proxy=127.0.0.1,10.0.2.15,localhost"            >> /etc/profile.d/proxy.sh
 
 # environment
-echo "export http_proxy=http://one.proxy.att.com:8080"          >> /etc/environment
-echo "export https_proxy=http://one.proxy.att.com:8080"         >> /etc/environment
-echo "export no_proxy=127.0.0.1,10.0.2.15,localhost"            >> /etc/environment
+#echo "export http_proxy=http://one.proxy.att.com:8080"          >> /etc/environment
+#echo "export https_proxy=http://one.proxy.att.com:8080"         >> /etc/environment
+#echo "export no_proxy=127.0.0.1,10.0.2.15,localhost"            >> /etc/environment
 
 echo " " >> /etc/hosts
 echo "10.0.2.15 controller" >> /etc/hosts
